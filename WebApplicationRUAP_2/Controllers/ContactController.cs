1f8c3f36c318eac9994251183eb7b8f65d5b1c86
Nikola <mdamjanovic2010@gmail.com>
2022-11-11 18:17:01 +0100

    Extracting Functionality into a Service Layer

--- WebApplicationRUAP_2/Controllers/ContactController.cs
+++ WebApplicationRUAP_2/Controllers/ContactController.cs
@@ -5,26 +5,21 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using WebApplicationRUAP_2.Models;
+using WebApplicationRUAP_2.Models.Services;
 
 namespace WebApplicationRUAP_2.Controllers
 {
     public class ContactController : ApiController
     {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
         public Contact[] Get()
         {
-            return new Contact[]
-            {
-        new Contact
-        {
-            Id = 1,
-            Name = "Glenn Block"
-        },
-        new Contact
-        {
-            Id = 2,
-            Name = "Dan Roth"
-        }
-            };
+            return contactRepository.GetAllContacts();
         }
     }
 }
